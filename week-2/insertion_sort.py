234f3614d075799820969619270da886f357b640
hellyab <yabsra23@gmail.com>
2021-01-04 20:01:57 +0300

    feat: implement insertion sort

---
+++ week-2/insertion_sort.py
@@ -0,0 +1,10 @@
+def insertion_sort(items):
+    for i in range(len(items)):
+        current_item = items[i]
+        for j in range(i, -1, -1):
+            if current_item < items[j]:
+                items[j+1] = items[j]
+            items[j] = current_item   
+    print(items)
+
+insertion_sort([5,4,3,2,1])
