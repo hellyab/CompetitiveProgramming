2641751f6229542ff3828f2870a5d88ec8d72da5
hellyab <yabsra23@gmail.com>
2021-01-13 16:59:13 +0300

    feat: implement maximum toys using python's built in sort function to avoid the time complexity

--- week-2/maximum_toys.py
+++ week-2/maximum_toys.py
@@ -6,28 +6,12 @@ import random
 import re
 import sys
 
-def counting_sort(items):
-    max = 0
-    for i in range(len(items)):
-        if items[i] > max:
-            max = items[i]
-    count_list = []
-    for i in range(max+1):
-        count_list.append(0)
-    for i in range(len(items)):
-        count_list[items[i]]+=1
-    final_list = []
-    for i in range(len(count_list)):
-        for j in range(count_list[i]):
-            final_list.append(i)
-    return final_list
-
 # Complete the maximumToys function below.
 def maximumToys(prices, k):
-    sorted_prices = insertion_sort(prices)
+    prices.sort()
     total = 0
     ctr = 0
-    while total + prices[ctr]<=k:
+    while total + prices[ctr] <= k and ctr<len(prices):
         total += prices[ctr]
         ctr +=1
     return ctr
