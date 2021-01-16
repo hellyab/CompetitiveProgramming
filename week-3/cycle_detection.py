b32c6ca8c1b1b14bc4cbdf283ccca1fca8b2f263
hellyab <yabsra23@gmail.com>
2021-01-16 16:31:47 +0300

    feat: implemented cycle detextionm using two pointers (slow and fast)

--- week-3/cycle_detection.py
+++ week-3/cycle_detection.py
@@ -8,3 +8,15 @@ def has_cycle(head):
         foundMap[head.__hash__()] = True
         head = head.next
     return 0
+
+def has_cycle2(head):
+    slow = head
+    fast = head
+    while fast:
+        if fast == slow:
+            return 1
+        if not fast.next:
+            return 0
+        fast = fast.next.next
+        slow = slow.next
+    return 0 
