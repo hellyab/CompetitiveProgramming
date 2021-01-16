0c5c425e43985e56ed3f4869742afc832891ff9d
hellyab <yabsra23@gmail.com>
2021-01-16 17:19:54 +0300

    feat: implemented find merge node using stack

--- week-3/find_merge_node.py
+++ week-3/find_merge_node.py
@@ -1,14 +1,28 @@
 def findMergeNode(head1, head2):
     foundIn1 = {}
-    ctr = 0
     while head1 or head2:
         print(head1.data, head2.data)
         if head2.__hash__() in foundIn1:
             return head2.data
         else:
             foundIn1[head1.__hash__()] = True
-            if ctr%2 == 0 and head1.next:
+            if head1.next:
                 head1 = head1.next
-            elif ctr%2 == 0 and head2.next:
+            elif head2.next:
                 head2 = head2.next
-        ctr += 1
+
+def findMergeNode2(head1, head2):
+    h1_stack = []
+    h2_stack = []
+    while head1:
+        h1_stack.append(head1)
+        head1 = head1.next
+    while head2:
+        h2_stack.append(head2)
+        head2 = head2.next
+    merge_node = h2_stack[-1]
+    while h2_stack and h1_stack and h1_stack[-1] == h2_stack[-1] :
+        merge_node = h1_stack[-1]
+        h1_stack.pop()
+        h2_stack.pop()
+    return merge_node.data
